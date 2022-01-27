6ce6a993d6d486f45bbb6766e62b0af02e895df8
Quang <tvq.dev@gmail.com>
2022-01-27 13:04:36 +0700

    sua lai thu muc

--- home/controllers/main.js
+++ home/controllers/main.js
@@ -1,23 +1,23 @@
+let sp = new ProductService();
+
 function getDataUI() {
-     axios({
-          method: 'GET',
-          url: 'https://61d03ed8cd2ee50017cc980b.mockapi.io/QLSP'
-      })
+     sp.getDataProduct()
           .then(function (result) {
-              hienThiDS(result.data);
+               hienThiDS(result.data);
+               search(result.dataProduct)
           })
           .catch(function (error) {
-              console.log(error);
+               console.log(error);
           });
-  }
+}
 getDataUI();
-  
+
 function hienThiDS(mangSP) {
      console.log(mangSP);
      var content = "";
      mangSP.map(function (sp, index) {
-         // content = content + "col-4"
-         content += `
+          // content = content + "col-4"
+          content += `
          <div class="product-item">
                          <div class="product-img">
                               <img src=${sp.img} alt="">
@@ -37,9 +37,25 @@ function hienThiDS(mangSP) {
          `
      });
      document.getElementById("product-content").innerHTML = content;
- 
- }
 
- function seachSP() {
-      
- }
+}
+
+function search(id) {
+     var dataProduct = [];
+     sp.getDataSearch(id)
+          .then(function (result) {
+               var datas = result.data.type;
+               console.log([result.data.type]);
+               for (let i = 0; i < datas.length; i++) {
+                    if (datas === "Iphone" || datas === "Samsung") {
+                         dataProduct.push(datas[i]);
+               hienThiDS(result.data);
+                         
+                    }               
+               }
+               hienThiDS(result.data);
+          })
+          .catch(function (error) {
+               console.log(error);
+          });  
+}
