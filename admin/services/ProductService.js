716f7e1a7acfe2d75b8b59185a0a2a8fe86934c1
Long <dangtienlong2020@gmail.com>
2022-02-14 23:16:05 +0700

    up code xoa sua

--- admin/services/ProductService.js
+++ admin/services/ProductService.js
@@ -17,9 +17,22 @@ function ProductService() {
    
      this.xoaSP = function(id){
        return axios({
-           method: "post",
-           url: "https://61d03ed8cd2ee50017cc980b.mockapi.io/QLSP",
+           method: "delete",
+           url: `https://61d03ed8cd2ee50017cc980b.mockapi.io/QLSP/${id}`,
          });
      }
+     this.xemSP = function (id){
+      return axios({
+        method: "get",
+        url: `https://61d03ed8cd2ee50017cc980b.mockapi.io/QLSP/${id}`,
+      });
+     };
+     this.capNhatSP = function(id, sp){
+      return axios({
+        method: "put",
+        url: `https://61d03ed8cd2ee50017cc980b.mockapi.io/QLSP/${id}`,
+        data: sp,
+      });
+     };
    }
    
